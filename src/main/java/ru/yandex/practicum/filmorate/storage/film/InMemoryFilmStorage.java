b8147f3ac8532f48aebb588c1aa37dd7d4108c43
Eugene Bukhanov <buhanza21@yandex.ru>
2023-09-01 20:22:34 +0300

    refactoring: storage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -3,33 +3,49 @@ package ru.yandex.practicum.filmorate.storage.film;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.UserException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.UuidGenerator;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Getter
 @Repository
 public class InMemoryFilmStorage implements FilmStorage {
-    private final Map<String, Film> movies;
+    private final Map<Integer, Film> films;
     private final UuidGenerator uuidGenerator;
 
     @Autowired
     public InMemoryFilmStorage(UuidGenerator uuidGenerator) {
-        this.movies = new HashMap<>();
+        this.films = new HashMap<>();
         this.uuidGenerator = uuidGenerator;
     }
 
     public Film add(Film film) {
-        final String id = uuidGenerator.nextUuid();
+        if (getFilms().containsKey(film.getId())) {
+            throw new UserException("The user is already in the database");
+        }
+
+        final int id = uuidGenerator.nextFilmId();
 
         film.setId(id);
-        movies.put(id, film);
+        films.put(id, film);
         return film;
     }
 
     public void put(Film film) {
-        movies.put(film.getId(), film);
+        if (!getFilms().containsKey(film.getId())) {
+            throw new UserException("The user isn't already in the database");
+        }
+        films.put(film.getId(), film);
+    }
+
+    @Override
+    public Film getFilm(int id) {
+        return films.get(id);
+    }
+
+    public List<Film> getSortedTopTenFilms() {
+        return new ArrayList<>(films.values());
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,18 +1,21 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.UserException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UuidGenerator;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Getter
 @Repository
 public class InMemoryUserStorage implements UserStorage {
-    protected final Map<String, User> users;
+    protected final Map<Integer, User> users;
     private final UuidGenerator uuidGenerator;
 
     @Autowired
@@ -22,13 +25,34 @@ public class InMemoryUserStorage implements UserStorage {
     }
 
     public User add(User user) {
-        final String id = uuidGenerator.nextUuid();
+        if (getUsers().containsKey(user.getId())) {
+            throw new UserException("The user is already in the database");
+        }
+
+        final int id = uuidGenerator.nextUserId();
+
         user.setId(id);
         users.put(id, user);
         return user;
     }
 
     public void put(User user) {
+        if (!getUsers().containsKey(user.getId())) {
+            throw new UserException("The user isn't already in the database");
+        }
+
         users.put(user.getId(), user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return users.get(id);
+    }
+
+    public void setUserNameByLogin(User user, String text) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        log.debug("The user is {}: {}", text, user.getName());
     }
 }
