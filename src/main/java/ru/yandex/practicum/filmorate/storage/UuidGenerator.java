bb8483298dc98aa41f9793d98ddd3b98b44810af
Eugene Bukhanov <buhanza21@yandex.ru>
2023-09-01 20:22:46 +0300

    feat: generator id

--- src/main/java/ru/yandex/practicum/filmorate/storage/UuidGenerator.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UuidGenerator.java
@@ -6,7 +6,17 @@ import java.util.UUID;
 
 @Component
 public class UuidGenerator {
+    private int userId = 1;
+    private int filmId = 1;
     public String nextUuid() {
         return UUID.randomUUID().toString();
+    }
+
+    public int nextUserId() {
+        return userId++;
+    }
+
+    public int nextFilmId() {
+        return filmId++;
     }
 }
