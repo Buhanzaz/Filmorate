44206987a086c26e146aa73d64d843872e351d8e
Eugene Bukhanov <buhanza21@yandex.ru>
2023-09-01 20:22:12 +0300

    feat: add new interface

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,11 +1,14 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Map;
 
+@Component
 public interface FilmStorage {
     Film add(Film film);
     void put(Film film);
-    Map<String, Film> getMovies();
+    Map<Integer, Film> getFilms();
+    Film getFilm(int id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,12 +1,16 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Map;
 
+@Component
 public interface UserStorage {
     User add(User user);
     void put(User user);
-    Map<String, Film> getUsers();
+    Map<Integer, User> getUsers();
+    User getUser(int id);
+    void setUserNameByLogin(User user, String text);
 }
