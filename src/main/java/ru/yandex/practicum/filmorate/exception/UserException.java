faf473e1177e53805c8955822ad90ea83b2ce823
Eugene Bukhanov <buhanza21@yandex.ru>
2023-09-01 23:24:58 +0300

    codeStyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/UserException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/UserException.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 public class UserException extends RuntimeException {
     String error;
     String description;
+
     public UserException(String message) {
         super(message);
     }
