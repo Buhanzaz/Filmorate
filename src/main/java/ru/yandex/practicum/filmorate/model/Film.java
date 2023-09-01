f988fcf22ba2c60e6ca67681cd8b2132e8541ae2
Eugene Bukhanov <buhanza21@yandex.ru>
2023-09-01 20:17:25 +0300

    refactoring: changed Film and User model

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,18 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
 import javax.validation.constraints.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import ru.yandex.practicum.filmorate.annotation.AfterDate;
+import ru.yandex.practicum.filmorate.exception.FilmException;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
-@AllArgsConstructor
 public class Film {
-    //@PositiveOrZero(message = "Id cannot be a negative number")
-    private String id;
+    @PositiveOrZero(message = "Id cannot be a negative number")
+    private int id;
 
     @NotBlank(message = "Name film is mandatory")
     private final String name;
@@ -26,4 +28,33 @@ public class Film {
     @NotNull(message = "Duration film is not empty")
     @PositiveOrZero(message = "Duration cannot be a negative number")
     private final int duration;
+
+    @Setter(AccessLevel.NONE)
+    private Set<Integer> usersWhoLikeIt = new HashSet<>();
+
+    @Setter(AccessLevel.NONE)
+    private int likes = 0;
+
+    @SneakyThrows
+    public void addLike(int idUser) {
+        try {
+                usersWhoLikeIt.add(idUser);
+                likes++;
+        } catch (Exception e) {
+            throw new FilmException("Have you already liked this movie");
+        }
+    }
+
+    @SneakyThrows
+    public void removeLike(int idUser) {
+        try {
+            if (usersWhoLikeIt.contains(idUser)) {
+                usersWhoLikeIt.remove(idUser);
+                likes--;
+            }
+        } catch (Exception e) {
+            throw new FilmException("Have you already liked this movie");
+        }
+    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,15 +2,19 @@ package ru.yandex.practicum.filmorate.model;
 
 import javax.validation.constraints.*;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class User {
 
-    //@PositiveOrZero(message = "Id cannot be a negative number")
-    private String id;
+    @PositiveOrZero(message = "Id cannot be a negative number")
+    private int id;
 
     @NotBlank
     @NotNull
@@ -25,4 +29,15 @@ public class User {
 
     @PastOrPresent
     private final LocalDate birthday;
+
+    @Setter(AccessLevel.NONE)
+    private Set<Integer> idFriends = new HashSet<>();
+
+    public void addFriend(int idUser) {
+        idFriends.add(idUser);
+    }
+
+    public void removeFriend(int idUser) {
+        idFriends.remove(idUser);
+    }
 }
