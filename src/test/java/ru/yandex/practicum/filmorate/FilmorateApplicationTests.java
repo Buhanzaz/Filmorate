9048ddb7f3a623833ba188194733ac0f51f48951
Eugene Bukhanov <128382088+Buhanzaz@users.noreply.github.com>
2023-10-06 02:33:49 +0300

    Update FilmorateApplicationTests.java

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -28,123 +28,123 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class FilmorateApplicationTests {
-	private final FilmDbStorage filmDbStorage;
-	private final RatingMpaDbStorage ratingMpaDbStorage;
-	private final UserDbStorage userDbStorage;
-	private Film film;
-	private Film secondFilm;
-	private User user;
-	private User secondUser;
+    private final FilmDbStorage filmDbStorage;
+    private final RatingMpaDbStorage ratingMpaDbStorage;
+    private final UserDbStorage userDbStorage;
+    private Film film;
+    private Film secondFilm;
+    private User user;
+    private User secondUser;
 
-	@BeforeEach
-	void setUp() {
+    @BeforeEach
+    void setUp() {
 
-		film = Film.builder()
-				.id(1)
-				.name("Test")
-				.description("test description")
-				.releaseDate(LocalDate.now().minusYears(40))
-				.duration(180)
-				.genres(new TreeSet<>())
-				.mpa(ratingMpaDbStorage.getRatingMpaById(1))
-				.build();
+        film = Film.builder()
+                .id(1)
+                .name("Test")
+                .description("test description")
+                .releaseDate(LocalDate.now().minusYears(40))
+                .duration(180)
+                .genres(new TreeSet<>())
+                .mpa(ratingMpaDbStorage.getRatingMpaById(1))
+                .build();
 
-		secondFilm = Film.builder()
-				.id(1)
-				.name("Test 2")
-				.description("test description 2")
-				.releaseDate(LocalDate.now().minusYears(40))
-				.duration(180)
-				.genres(new TreeSet<>()).mpa(ratingMpaDbStorage.getRatingMpaById(1))
-				.build();
+        secondFilm = Film.builder()
+                .id(1)
+                .name("Test 2")
+                .description("test description 2")
+                .releaseDate(LocalDate.now().minusYears(40))
+                .duration(180)
+                .genres(new TreeSet<>()).mpa(ratingMpaDbStorage.getRatingMpaById(1))
+                .build();
 
-		user = User.builder()
-				.id(1)
-				.name("Eugene")
-				.login("Buhanzaz")
-				.email("Buhanza21@yandex.ru")
-				.birthday(LocalDate.now().minusYears(30))
-				.build();
+        user = User.builder()
+                .id(1)
+                .name("Eugene")
+                .login("Buhanzaz")
+                .email("Buhanza21@yandex.ru")
+                .birthday(LocalDate.now().minusYears(30))
+                .build();
 
-		secondUser = User.builder()
-				.id(2)
-				.name("Eugene")
-				.login("Buhanzaz")
-				.email("Buhanza21@gmail.ru")
-				.birthday(LocalDate.now().minusYears(30))
-				.build();
-	}
+        secondUser = User.builder()
+                .id(2)
+                .name("Eugene")
+                .login("Buhanzaz")
+                .email("Buhanza21@gmail.ru")
+                .birthday(LocalDate.now().minusYears(30))
+                .build();
+    }
 
-	@Test
-	void getAllFilms_shouldConfirmThatTwoFilmsWasAddedAtList() {
-		filmDbStorage.create(film);
-		filmDbStorage.create(secondFilm);
-		Collection<Film> films = filmDbStorage.getAllFilm();
+    @Test
+    void getAllFilms_shouldConfirmThatTwoFilmsWasAddedAtList() {
+        filmDbStorage.create(film);
+        filmDbStorage.create(secondFilm);
+        Collection<Film> films = filmDbStorage.getAllFilm();
 
-		assertThat(films).hasSize(2);
-	}
+        assertThat(films).hasSize(2);
+    }
 
-	@Test
-	void createFilm_shouldConfirmThatFilmIdExists() {
-		filmDbStorage.create(film);
-		Film filmOptional = filmDbStorage.getById(1);
+    @Test
+    void createFilm_shouldConfirmThatFilmIdExists() {
+        filmDbStorage.create(film);
+        Film filmOptional = filmDbStorage.getById(1);
 
-		assertEquals(filmOptional.getId(), 1);
-	}
+        assertEquals(filmOptional.getId(), 1);
+    }
 
-	@Test
-	void getFilmById_shouldConfirmThatFilmIdExists() {
-		filmDbStorage.create(film);
+    @Test
+    void getFilmById_shouldConfirmThatFilmIdExists() {
+        filmDbStorage.create(film);
 
-		assertEquals(filmDbStorage.getById(1).getId(),film.getId());
-	}
+        assertEquals(filmDbStorage.getById(1).getId(), film.getId());
+    }
 
-	@Test
-	public void getAllUsers_shouldConfirmThatTwoUsersWasAddedAtList() {
-		userDbStorage.create(user);
-		userDbStorage.create(secondUser);
-		Collection<User> users = userDbStorage.getAll();
+    @Test
+    public void getAllUsers_shouldConfirmThatTwoUsersWasAddedAtList() {
+        userDbStorage.create(user);
+        userDbStorage.create(secondUser);
+        Collection<User> users = userDbStorage.getAll();
 
-		assertThat(users).contains(user);
-		assertThat(users).contains(secondUser);
-	}
+        assertThat(users).contains(user);
+        assertThat(users).contains(secondUser);
+    }
 
-	@Test
-	public void createUser_shouldConfirmThatUserIdExists() {
-		userDbStorage.create(user);
-		User userOptional = userDbStorage.getById(1);
+    @Test
+    public void createUser_shouldConfirmThatUserIdExists() {
+        userDbStorage.create(user);
+        User userOptional = userDbStorage.getById(1);
 
-		assertEquals(userOptional.getId(), 1);
-	}
+        assertEquals(userOptional.getId(), 1);
+    }
 
-	@Test
-	public void getUserById_shouldConfirmThatUserNameExists() {
-		userDbStorage.create(user);
-		User userOptional = userDbStorage.getById(1);
+    @Test
+    public void getUserById_shouldConfirmThatUserNameExists() {
+        userDbStorage.create(user);
+        User userOptional = userDbStorage.getById(1);
 
-		assertEquals(userOptional.getName(), "Eugene");
-	}
+        assertEquals(userOptional.getName(), "Eugene");
+    }
 
-	@Test
-	public void deleteUserById_ShouldConfirmThatUsernameHasBeenDeleted() {
-		userDbStorage.create(user);
-		userDbStorage.delete(user.getId());
-		NotFoundException exception = assertThrows(NotFoundException.class, this::execute);
+    @Test
+    public void deleteUserById_ShouldConfirmThatUsernameHasBeenDeleted() {
+        userDbStorage.create(user);
+        userDbStorage.delete(user.getId());
+        NotFoundException exception = assertThrows(NotFoundException.class, this::execute);
 
-		assertEquals(exception.getMessage(), "User with ID=1 not found!");
-	}
+        assertEquals(exception.getMessage(), "User with ID=1 not found!");
+    }
 
-	@Test
-	
-	public void deleteFilmById_ShouldConfirmThatUsernameHasBeenDeleted() {
-		filmDbStorage.create(film);
-		filmDbStorage.delete(film.getId());
-		NotFoundException exception = assertThrows(NotFoundException.class, () -> filmDbStorage.getById(film.getId()));
+    @Test
 
-		assertEquals(exception.getMessage(), "Movie with ID = 1 not found!");
-	}
+    public void deleteFilmById_ShouldConfirmThatUsernameHasBeenDeleted() {
+        filmDbStorage.create(film);
+        filmDbStorage.delete(film.getId());
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> filmDbStorage.getById(film.getId()));
 
-	private void execute() {
-		userDbStorage.getById(user.getId());
-	}
+        assertEquals(exception.getMessage(), "Movie with ID = 1 not found!");
+    }
+
+    private void execute() {
+        userDbStorage.getById(user.getId());
+    }
 }
