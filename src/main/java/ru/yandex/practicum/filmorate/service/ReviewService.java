d0c9ad8e67fe874be06b1b996ca4066a1867b226
Nucleus1337 <nucleus1337@yandex.ru>
2023-10-07 17:36:31 +0300

    add-reviews feat: добавить проверки, что пользователь или отзыв существует на операции "Добавление лайка", "Удаление лайка", "Удаление отзыва" fix: поменять язык логов; исправить проверки пользователей и фильмов (перепутаны ID)

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -28,57 +28,75 @@ public class ReviewService {
     }
 
     private void checkUserOrFilmExists(Integer userId, Integer filmId) {
-        log.info("Проверяем существование пользователя с ID = {}", userId);
-        filmStorage.getById(userId);
-        log.info("Проверяем существование фильма с ID = {}", userId);
-        userStorage.getById(filmId);
+        log.info("Check if USER with ID = {} exists", userId);
+        userStorage.getById(userId);
+        log.info("Check if FILM with ID = {} exists", userId);
+        filmStorage.getById(filmId);
     }
 
     public Review add(Review review) {
         checkUserOrFilmExists(review.getUserId(), review.getFilmId());
 
-        log.info("Пытаемся добавить новый отзыв: {}", review);
+        log.info("Trying to add REVIEW: {}", review);
         return reviewStorage.add(review);
     }
 
     public Review findById(Integer id) {
         try {
-            log.info("Ищем отзыв с ID = {}", id);
+            log.info("Find REVIEW with ID = {} exists", id);
             return reviewStorage.findById(id);
         } catch (EmptyResultDataAccessException e) {
-            throw new NotFoundException(String.format("Отзыв с ID = %s не существует", id));
+            throw new NotFoundException(String.format("REVIEW with ID = %s does not exists", id));
         }
     }
 
     public List<Review> findAllOrByFilmId(String id, String count) {
         int intCount = Integer.parseInt(count);
         if (id == null) {
-            log.info("Полчаем отзывы по фильмам");
+            log.info("Get REVIEWS for all FILMS");
             return reviewStorage.findAll();
         } else {
             int intId = Integer.parseInt(id);
-            log.info("Получаем отзывы по фильму с ID = {}", intId);
+            log.info("Get REVIEWS for FILM with ID = {}", intId);
             return reviewStorage.findAllByFilmId(intId, intCount);
         }
     }
 
     public Review update(Review review) {
-        log.info("Обновляем фильм с ID = {}", review.getReviewId());
+        log.info("Check if REVIEW with ID = {} exists", review.getReviewId());
+        findById(review.getReviewId());
+
+        log.info("Update REVIEW with ID = {}", review.getReviewId());
         return reviewStorage.update(review);
     }
 
     public Review addLike(Integer id, Integer userId, int grade) {
-        log.info("Пользователь ID = {} отгеагировал ({}) на отзыв ID = {}", userId, grade, id);
+        checkUserAndLikeExists(id, userId);
+
+        log.info("USER with ID = {} rate ({}) REVIEW with ID = {}", userId, grade, id);
         return reviewStorage.addLike(id, userId, grade);
     }
 
     public Review removeLike(Integer id, Integer userId) {
-        log.info("Удаляем отзыв ID = {} от пользователя ID = {}", id, userId);
+        checkUserAndLikeExists(id, userId);
+
+        log.info("Remove REVIEW with ID = {} from USER with ID = {}", id, userId);
         return reviewStorage.removeLike(id, userId);
     }
 
     public void removeById(Integer id) {
-        log.info("Удаляем отзыв ID = {}", id);
+        log.info("Check if REVIEW with ID = {} exists", id);
+        findById(id);
+
+        log.info("Remove REVIEW with ID = {}", id);
         reviewStorage.removeById(id);
+    }
+
+    private void checkUserAndLikeExists(Integer reviewId, Integer userId) {
+        log.info("Check if REVIEW with ID = {} exists", reviewId);
+        findById(reviewId);
+
+        log.info("Check if USER with ID = {} exists", userId);
+        userStorage.getById(userId);
     }
 }
